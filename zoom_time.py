527126b8bb2d7411d09683d08e1fe4f29a548ab3
ndnd <51066110+nick-dab@users.noreply.github.com>
2022-01-13 19:27:12 -0500

    Update zoom_time.py

--- zoom_time.py
+++ zoom_time.py
@@ -1,59 +1,37 @@
-from icalendar import Calendar, Event
+"""
+doc: https://icalendar.readthedocs.io/en/latest/api.html?highlight=from_ical#module-icalendar.prop
+"""
+from icalendar import Calendar
 import time
 import re
 import webbrowser
 
 
-"""
-doc: https://icalendar.readthedocs.io/en/latest/api.html?highlight=from_ical#module-icalendar.prop
-"""
-
 # Replace 'test.ics' with proper ics file containing start time and url link under location
 
-def rm_chars(c):
-    return c.group(1)
+# set current time via time module
+time_now = time.strftime("%H:%M:%S")
 
-
-
-time_now = time.strftime('%H:%M:%S') # set current time via time module
-cal = open('test.ics') # opening the file, returning it as file object
+# opening the file, returning it as file object
+cal = open("test.ics")
 ccal = Calendar.from_ical(cal.read())
 
-# extract url
-for components in ccal.walk(): # iterating through each of the components in the data within calendar
-    if 'https' or 'http' in components: # doesn't work if i only put 'https', not sure why. needs 'http'
-        try:
-            url = (re.findall(r'(https?://[^\s]+)', str(components))[0]) # searching for any string containing the url
-            break
-        except Exception as IndexError: # exception throws for some reason but program still runs. this catches it
-            result = ''
-    else:
-        print("No URL exists\n")
+for event in ccal.walk("vEvent"):
+    class_start_datetime = event.get("dtstart").dt
+    class_end_datetime = event.get("dtend").dt
+    url = str(event["location"])
 
-modurl = re.sub("(.*)(.{4}$)", rm_chars, url) # removing the extra 4 special characters at the end of the extracted url string
-print('Found URL:', modurl) # printing url to confirm
-
-# get start time
-for dt in ccal.walk('vEvent'):
-    class_start = dt.get('dtstart')
-    end = dt.get('dtend')
-
-    class_start2 = class_start.dt
-
-str_time = str(class_start2) # convert start time into str
-start_time = re.findall('(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)', str_time)[0] # get HH:MM:SS of start time via regex
-
-
-while (time_now < start_time):
-    print("Waiting until start time. Current time is:", time_now)
-    print("Class starts at:", start_time, "\n")
-    time_now = time.strftime('%H:%M:%S') # updating time after each check
-    time.sleep(2) # first check seems to be the same as initial time, keeping sleep count low could help prevent lateness
+while time_now < str(class_end_datetime):
+     print("Waiting until start time. Current time is:", time_now)
+     print("Class starts at:", class_start_datetime, "\n")
+     # updating time after each check
+     time_now = time.strftime("%H:%M:%S")
+     time.sleep(2)
+     # first check seems to be the same as initial time, keeping sleep count low could help prevent lateness
 
 # open extracted url
-if (time_now >= start_time):
-    print("Class started - Opening now...")
-    webbrowser.open(modurl) # web browser module to open url via default browser
-
-
+if time_now >= str(class_start_datetime):
+     print("Class started - Opening now...")
+     # web browser module to open url via default browser
+     webbrowser.open(url)
 
